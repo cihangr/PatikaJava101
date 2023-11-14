a31e5b2949ee0af8e9b4829172771847a44e8fe7
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 12:09:09 +0200

    Update Main.java

--- artikyilhesabi-main/src/Main.java
+++ artikyilhesabi-main/src/Main.java
@@ -1,17 +1,19 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+package artikYilHesaplama;
+import java.util.Scanner;
+
+public class artikYilHesaplama {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int year ;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hesaplanacak Yılı Giriniz : ");
+        year = input.nextInt();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        if (year %4 == 0 && year %100 != 0 ||  year %400 == 0 ) {
+            System.out.println( year +" Artık Yıldır");
+
+        }else {
+            System.out.println(year +" Artık Yıl Değildir");
         }
     }
 }
