6b9b8e4235e0c87e8b825eec3f569157cbe66bce
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 08:31:52 +0200

    Update Main.java

--- taksimetrehesaplatma-main/src/Main.java
+++ taksimetrehesaplatma-main/src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int km;
+        double perKm = 2.20;
+        double tutar=10 ;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Mesafeyi Giriniz :");
+        km = input.nextInt();
+        tutar += (km * perKm) ;
+        tutar = (tutar < 20) ? 20 : tutar ;
+        System.out.println("Toplam tutar : " + tutar);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
