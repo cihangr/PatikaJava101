f672fc8fab6fcd7ddeaa236fbb3ff5e669d87519
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 11:30:24 +0200

    Update Main.java

--- fibonacci-series-main/src/Main.java
+++ fibonacci-series-main/src/Main.java
@@ -1,17 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int n, firstTerm = 0, secondTerm = 1;
+        Scanner sc = new Scanner(System.in);
+        n= sc.nextInt();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + " ");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
         }
     }
 }
