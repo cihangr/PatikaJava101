c38870817474e0bf7296848a9936f780a722c717
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 11:41:32 +0200

    Update Main.java

--- dizi-harmonik-ortamasi-main/src/Main.java
+++ dizi-harmonik-ortamasi-main/src/Main.java
@@ -1,17 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        int[] list ={1,3,4,6,7,8,12,13,65,23,67,99,84,14};
+        int sum=0;
+        for (int i=0; i<list.length; i++){
+            sum+=list[i];
         }
+        System.out.println(sum/list.length);
     }
 }
