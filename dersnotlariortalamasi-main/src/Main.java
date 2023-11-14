92ec3291db5cd5b7a02655cdd78de00d5140b9f7
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 11:47:04 +0200

    Update Main.java

--- dersnotlariortalamasi-main/src/Main.java
+++ dersnotlariortalamasi-main/src/Main.java
@@ -1,17 +1,36 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package ortalamaHesaplama;
+import java.util.Scanner;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class ortalamaHesaplama {
+    public  static void main(String[] args) {
+        int mat, fizik, kimya, tarih, muzik, turkce;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Matematik Ders Notunu Griniz .: ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Ders Notunu Griniz .: ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Ders Notunu Griniz : ");
+        kimya = inp.nextInt();
+
+        System.out.print("Tarih Ders Notunu Griniz .: ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Ders Notunu Griniz .: ");
+        muzik = inp.nextInt();
+
+        System.out.print("Türkçe Ders Notunu Griniz .: ");
+        turkce = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + tarih + muzik + turkce);
+        double sonuc = (toplam / 6.0);
+
+        System.out.println("Ders ORTALAMANIZ .: " + sonuc);
+        System.out.println(sonuc < 60 ? "UZGUNUM KALDINIZ :(" : "TEBRIKLER GECTINIZ");
+
+
     }
 }
