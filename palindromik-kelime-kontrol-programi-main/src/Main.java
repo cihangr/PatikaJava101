337596ad9a22d6f4334df87627660c494f4ed278
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 11:18:01 +0200

    Update Main.java

--- palindromik-kelime-kontrol-programi-main/src/Main.java
+++ palindromik-kelime-kontrol-programi-main/src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+    public static boolean isPalindromik (String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+            }
+        return true;
         }
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Palindromik kelimeyi girin: ");
+        String str = inp.nextLine();
+        System.out.println(isPalindromik(str));
     }
 }
