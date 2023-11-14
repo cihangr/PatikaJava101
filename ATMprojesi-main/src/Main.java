e8fdc33e0adebcdd745ebb47d56e5dcd0ee56f6f
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 12:12:37 +0200

    Update Main.java

--- ATMprojesi-main/src/Main.java
+++ ATMprojesi-main/src/Main.java
@@ -1,17 +1,86 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner inp = new Scanner(System.in);
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        String userPassword,userName; // In system username and password: rraeich / 1803
+        int action,deposit,withdraw,balance=1700; // Default balance: 1700 Euro
+
+        System.out.print("Enter your username: ");
+        userName = inp.nextLine();
+
+        System.out.print("Enter your password: ");
+        userPassword = inp.nextLine();
+
+        if(!(userPassword.equals("1803") && userName.equals("rraeich"))) {
+            for(int i=0; i < 3 ;i++) {
+                System.out.println("Wrong username or password. Please try again.");
+                System.out.println("Your remaining attempts: "+(3-i));
+                System.out.print("Enter your username: ");
+                userName = inp.nextLine();
+
+                System.out.print("Enter your password: ");
+                userPassword = inp.nextLine();
+
+                if (userPassword.equals("1803") && userName.equals("rraeich")){
+                    break;
+                }
+
+                else if (i==2){
+                    System.out.print("Your bank account locked Please contact the bank.");
+                    return;
+                }
+            }
         }
+        do {
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("Welcome to RR Bank "+userName+"!");
+            System.out.println("\n1- To deposit Money\n" +
+                    "2- Withdraw Money\n" +
+                    "3- Ask for Account Balance\n" +
+                    "4- Quit the Account");
+            System.out.print("\nPlease select the action you want to take:");
+            action = inp.nextInt();
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            switch (action){
+
+                case 1:
+                    System.out.print("Enter the amount of money you want to deposit: ");
+                    deposit = inp.nextInt();
+                    balance += deposit;
+                    System.out.println("New Account balance: " + balance + "€");
+                    break;
+
+                case 2:
+                    System.out.print("Enter the amount of money you want to withdraw: ");
+                    withdraw = inp.nextInt();
+
+                    if (balance < withdraw){
+                        System.out.println("You dont have this much money. Please try again.");
+                        break;
+                    }
+
+                    else {
+                        balance = balance - withdraw;
+                        System.out.println("New Account balance: " + balance + "€");
+                        break;
+                    }
+
+                case 3:
+                    System.out.println("Your account balance: "+ balance + "€");
+                    break;
+
+                case 4:
+                    System.out.println("See you soon " +userName + "!");
+                    return;
+
+                default:
+                    System.out.println("Your action choosing not right. Quitting Account..");
+                    return;
+            }
+        } while (action != 4);
     }
 }
