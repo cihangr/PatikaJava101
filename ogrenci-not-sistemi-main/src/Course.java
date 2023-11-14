d66d6323ca6db387b09e6580cd04b9ccb42c742c
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 11:19:47 +0200

    Update Course.java

--- ogrenci-not-sistemi-main/src/Course.java
+++ ogrenci-not-sistemi-main/src/Course.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Course {
+//Course Sınıfı Özellikleri :
+//
+//Nitelikler : name,code,prefix,note,Teacher
+//Metotlar : Course() , addTeacher() , printTeacher()
+
+public class Course {
+    String name;
+    int code;
+    String prefix;
+    double note;
+    int verbalNote;
+    Teacher teacher;
+    int sNote;
+
+    public Course(String name, int code, String prefix) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+    }
+
+    public void addTeacher (Teacher teacher) {
+        if (teacher.branch.equals(this.prefix)){
+            this.teacher=teacher;
+            System.out.println(teacher.name+" ogretmen "+ this.prefix + " dersine atandi.");
+        }else {
+            System.out.println("Bu ogretmen farkli bir bransta calismaktir!");
+        }
+    }
+
+    public void printTeacher() {
+        if (teacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + teacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmamıştır.");
+        }
+    }
+
 }
+
