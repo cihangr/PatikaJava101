ffa9cdd4e20506ee14aff85ea21a6003914502c6
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 11:20:13 +0200

    Update Teacher.java

--- ogrenci-not-sistemi-main/src/Teacher.java
+++ ogrenci-not-sistemi-main/src/Teacher.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Teacher {
+//Teacher Sınıfı Özellikleri :
+//
+//Nitelikler : name,mpno,branch
+//Metotlar : Teacher()
+
+public class Teacher {
+    String name;
+    int mbno;
+    String branch;
+    public Teacher(String name, int mbno, String branch){
+        this.name = name;
+        this.mbno = mbno;
+        this.branch = branch;
+    }
+
+
 }
