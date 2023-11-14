ca70157bed9c6c4bc6badf7dd0aea7a14516382a
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 11:52:36 +0200

    Update Main.java

--- dairecevrealanhesabi-main/src/Main.java
+++ dairecevrealanhesabi-main/src/Main.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package daireAlaniHesaplama;
+import java.util.Scanner;
+public class daireAlaniHesaplama {
+    public static void main(String [] args){
+        int r,a;
+        double pi=3.14;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen Dairenin Yarı Çapını Giriniz : ");
+        r = input.nextInt();
+        System.out.print("Lütfen Merkez Açısını Giriniz : ");
+        a = input.nextInt();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        double alan = pi * r * r ;
+        double cevre = 2 * pi * r;
+        double dairedilimi = ((alan * a) / 360);
+
+        System.out.println("Dairenizin Alanı : " + alan);
+        System.out.println("Dairenizin Çevresi : " + cevre);
+        System.out.println("Dairenizin Dilimi Alanı : " + dairedilimi);
+
     }
 }
