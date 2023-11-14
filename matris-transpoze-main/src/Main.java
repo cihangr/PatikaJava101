4ac3a376a979b9e310b5016edb2473298385680a
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 11:22:19 +0200

    Update Main.java

--- matris-transpoze-main/src/Main.java
+++ matris-transpoze-main/src/Main.java
@@ -1,17 +1,35 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Lutfen Matrisin buyuklugunu giriniz M(k x j)");
+        System.out.print("k: ");
+        Scanner inp = new Scanner(System.in);
+        int k = inp.nextInt();
+        System.out.print("j: ");
+        int j = inp.nextInt();
+        int[][] A = new int[k][j];
+        int[][] T = new int[j][k];
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int row = 0; row < A.length; row++) {
+            for (int column = 0; column < A[row].length; column++) {
+                A[row][column] = inp.nextInt();
+                T[column][row] = A[row][column];
+            }
+        }
+        for (int i=0; i<A.length; i++){
+            for (int m=0; m<A[i].length; m++){
+                System.out.print(A[i][m]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("=====TRANSPOZE=====");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        for (int i=0; i<T.length; i++) {
+            for (int m = 0; m < T[i].length; m++) {
+                System.out.print(T[i][m] + " ");
+            }
+            System.out.println();
         }
     }
 }
