e015b0e36a9c6106dd569b3f4e84ac9bcf6af56b
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-14 08:26:08 +0200

    Update Main.java

--- ucakbiletifiyatihesaplama-main/src/Main.java
+++ ucakbiletifiyatihesaplama-main/src/Main.java
@@ -1,17 +1,86 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+package ucakbiletifiyatinormalTutar;
+import java.util.Scanner;
+
+public class UcakBiletiFiyatiHesaplama {
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        System.out.println("Uçak Bileti Fiyatı Hesaplama");
+        Scanner input = new Scanner(System.in);
+        
+        int mesafe, yas, yolculukTipi;
+        double normalTutar, yasIndirimi, indirimliTutar, gidisDönüsBiletIndirimi, toplamTutar;
+        System.out.print("Mesafe(KM) giriniz: ");
+        mesafe = input.nextInt();
+        
+        System.out.print("Lutfen yaşınızı giriniz: ");
+        yas = input.nextInt();
+        
+        System.out.print("Lutfen yolculuk tipi (1-Tek Yön, 2-Gidiş-Dönüş) giriniz: ");
+        yolculukTipi = input.nextInt();
+        normalTutar = mesafe * 0.10;
+                
+        if(yas>0 && yolculukTipi>0 && yolculukTipi<3)
+        {
+            if(yolculukTipi==1 && yas>=0)
+        {
+            if(yas<=12)
+            {
+                yasIndirimi = normalTutar * 0.50;
+                indirimliTutar = normalTutar - yasIndirimi;
+                System.out.print("İndirimli Toplam Fiyat: " + indirimliTutar);
+            }
+            if(yas>12 && yas<=24)
+            {
+                yasIndirimi = normalTutar * 0.10;
+                indirimliTutar = normalTutar - yasIndirimi;
+                System.out.print("İndirimli Toplam Fiyat: " + indirimliTutar);
+            }
+            if(yas>24)
+            {
+                System.out.print("Toplam Fiyat: " + normalTutar);
+            }
+            if(yas>=65)
+            {
+                yasIndirimi = normalTutar * 0.30;
+                indirimliTutar = normalTutar - yasIndirimi;
+                System.out.print("İndirimli Toplam Fiyat: " + indirimliTutar);
+            }
         }
-    }
+        if(yas>=0 && yolculukTipi==2)
+        {
+            if(yas<=12)
+            {
+                yasIndirimi = normalTutar * 0.50;
+                indirimliTutar = normalTutar - yasIndirimi;
+                gidisDönüsBiletIndirimi = indirimliTutar * 0.20;
+                toplamTutar = (indirimliTutar-gidisDönüsBiletIndirimi)*2;
+                System.out.print("İndirimli Toplam Fiyat: " + toplamTutar);
+            }
+            if(yas>12 && yas<=24)
+            {
+                yasIndirimi = normalTutar * 0.10;
+                indirimliTutar = normalTutar - yasIndirimi;
+                gidisDönüsBiletIndirimi = indirimliTutar * 0.20;
+                toplamTutar = (indirimliTutar-gidisDönüsBiletIndirimi)*2;
+                System.out.print("İndirimli Toplam Fiyat: " + toplamTutar);
+            }
+            if(yas<35 && yas>24)
+            {
+                System.out.print("Toplam Fiyat: " + normalTutar);
+            }
+            if(yas>=65)
+            {
+                yasIndirimi = normalTutar * 0.30;
+                indirimliTutar = normalTutar - yasIndirimi;
+                gidisDönüsBiletIndirimi = indirimliTutar * 0.20;
+                toplamTutar = (indirimliTutar-gidisDönüsBiletIndirimi)*2;
+                System.out.print("İndirimli Toplam Fiyat: " + toplamTutar);
+            }
+        }
+        }
+        else
+        {
+            System.out.println("Hatalı Veri Girdiniz !");
+        }
+    }  
 }
